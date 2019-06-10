e8620bae66ad21c359c8a53037815111cccde0e7
Yusuf Koer <ykoer@redhat.com>
2019-06-10 21:16:38 +0200

    Remove maven-surefire-plugin and fix unit test

--- src/test/java/de/ykoer/examples/hello/ServiceInfoControllerTest.java
+++ src/test/java/de/ykoer/examples/hello/ServiceInfoControllerTest.java
@@ -35,7 +35,7 @@ public class ServiceInfoControllerTest {
     @Test
     public void testPing() throws IOException {
         String response = serviceInfoController.ping();
-        assertThat(response).isEqualTo("pong");
+        assertThat(response).isEqualTo("Hello World!");
     }
 }
 
